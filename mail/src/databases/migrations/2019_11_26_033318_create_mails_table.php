d47edb40bb2200506d652647da585015771d75ea
canhht <canhht@scuti.asia>
2019-12-03 23:33:14 +0700

    Create base mail generator

---
+++ mail/src/databases/migrations/2019_11_26_033318_create_mails_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mails', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title');
+            $table->string('subject');
+            $table->text('content');
+            $table->string('attachment')->nullable();
+            $table->string('from');
+            $table->string('to');
+            $table->string('cc')->nullable();
+            $table->string('bcc')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('mails');
+    }
+}
